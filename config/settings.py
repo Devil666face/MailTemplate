ee258c9e7b95d6b3112ad1ae49aee5f62cf836e3
Devil666face <artem1999k@gmail.com>
2022-11-25 22:07:36 +0300

    add admin interface

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
